6d09dc8c1ab0b02758a225e7659781f724a7cf6c
klintcheng <klintcheng@163.com>
2021-08-30 16:19:25 +0800

    update server_test

--- examples/benchmark/server_test.go
+++ examples/benchmark/server_test.go
@@ -1,1 +1,95 @@
 package benchmark
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/klintcheng/kim"
+	"github.com/klintcheng/kim/examples/mock"
+	"github.com/klintcheng/kim/logger"
+	"github.com/klintcheng/kim/websocket"
+	"github.com/panjf2000/ants/v2"
+)
+
+func Test_Parallel(t *testing.T) {
+	const count = 10000
+	gpool, _ := ants.NewPool(50, ants.WithPreAlloc(true))
+	defer gpool.Release()
+	var wg sync.WaitGroup
+	wg.Add(count)
+
+	clis := make([]kim.Client, count)
+	t0 := time.Now()
+	for i := 0; i < count; i++ {
+		idx := i
+		_ = gpool.Submit(func() {
+			cli := websocket.NewClient(fmt.Sprintf("test_%v", idx), "client", websocket.ClientOptions{
+				Heartbeat: kim.DefaultHeartbeat,
+			})
+			// set dialer
+			cli.SetDialer(&mock.WebsocketDialer{})
+
+			// step2: 建立连接
+			err := cli.Connect(wsurl)
+			if err != nil {
+				logger.Error(err)
+			}
+			clis[idx] = cli
+			wg.Done()
+		})
+	}
+	wg.Wait()
+	t.Logf("logined %d cost %v", count, time.Since(t0))
+	t.Logf("done connecting")
+	time.Sleep(time.Second * 30)
+	t.Logf("closed")
+
+	for i := 0; i < count; i++ {
+		clis[i].Close()
+	}
+}
+
+func Test_Message(t *testing.T) {
+	const count = 10000
+	gpool, _ := ants.NewPool(50, ants.WithPreAlloc(true))
+	defer gpool.Release()
+
+	cli := websocket.NewClient(fmt.Sprintf("test_%v", 1), "client", websocket.ClientOptions{
+		Heartbeat: kim.DefaultHeartbeat,
+	})
+	// set dialer
+	cli.SetDialer(&mock.WebsocketDialer{})
+
+	// step2: 建立连接
+	err := cli.Connect(wsurl)
+	if err != nil {
+		logger.Error(err)
+	}
+	msg := []byte(strings.Repeat("hello", 1000))
+	t0 := time.Now()
+	for i := 0; i < count; i++ {
+		_ = gpool.Submit(func() {
+			_ = cli.Send(msg)
+		})
+	}
+	recv := 0
+	for {
+		frame, err := cli.Read()
+		if err != nil {
+			logger.Info(err)
+			break
+		}
+		if frame.GetOpCode() != kim.OpBinary {
+			continue
+		}
+		recv++
+		if recv == count { // 接收完消息
+			break
+		}
+	}
+
+	t.Logf("message %d cost %v", count, time.Since(t0))
+}
--- go.mod
+++ go.mod
@@ -29,7 +29,7 @@ require (
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
 	google.golang.org/grpc v1.33.2
 	google.golang.org/protobuf v1.25.0
 	gorm.io/driver/mysql v1.1.1
