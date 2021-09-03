1b0b917b822e47940a7b422e8195deb8077f6e83
klintcheng <klintcheng@163.com>
2021-09-03 23:20:47 +0800

    update Test_Message

--- examples/benchmark/server_test.go
+++ examples/benchmark/server_test.go
@@ -56,9 +56,6 @@ func Test_Parallel(t *testing.T) {
 
 func Test_Message(t *testing.T) {
 	const count = 50000
-	gpool, _ := ants.NewPool(100, ants.WithPreAlloc(true))
-	defer gpool.Release()
-
 	cli := websocket.NewClient(fmt.Sprintf("test_%v", 1), "client", websocket.ClientOptions{
 		Heartbeat: kim.DefaultHeartbeat,
 	})
@@ -72,16 +69,16 @@ func Test_Message(t *testing.T) {
 	}
 	msg := []byte(strings.Repeat("hello", 1000))
 	t0 := time.Now()
-	for i := 0; i < count; i++ {
-		_ = gpool.Submit(func() {
+	go func() {
+		for i := 0; i < count; i++ {
 			_ = cli.Send(msg)
-		})
-	}
+		}
+	}()
 	recv := 0
 	for {
 		frame, err := cli.Read()
 		if err != nil {
-			logger.Info(err)
+			logger.Info("time", time.Now().UnixNano(), err)
 			break
 		}
 		if frame.GetOpCode() != kim.OpBinary {
